fbbfcc715fe5d11648c4828a0ef5918073c63882
patience111 <scpeiyao@gmail.com>
2022-11-14 17:59:47 +0800

    argnet_ssaa_chunk edit

--- scripts/argnet_ssaa_chunk.py
+++ scripts/argnet_ssaa_chunk.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import LabelBinarizer
 from tensorflow.keras.utils import to_categorical
 import random
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import tqdm
 
 #load model
@@ -83,84 +83,102 @@ def prediction(seqs):
 def filter_prediction_batch(seqs):
         
     predictions = []
-    temp = filterm.predict(seqs, batch_size = 10240)
+    temp = filterm.predict(seqs, batch_size=8192)
     predictions.append(temp)
         
     return predictions
 
 def reconstruction_simi(pres, ori):
     simis = []
-    reconstructs = []
-    for index, ele in enumerate(pres[0]):
+    #reconstructs = []
+    argmax_pre = np.argmax(pres[0], axis=2)
+    for index, ele in enumerate(argmax_pre):
         length = len(ori[index])
         count_simi = 0
-        reconstruct = ''
+        #reconstruct = ''
         for pos in range(length):
-            if chars[np.argmax(ele[pos])] == ori[index][pos]:
+            if chars[ele[pos]] == ori[index][pos]:
                 count_simi += 1
-            reconstruct += chars[np.argmax(ele[pos])]
+            #reconstruct += chars[np.argmax(ele[pos])]
         simis.append(count_simi / length)
-        reconstructs.append(reconstruct)
-    return reconstructs, simis
+        #reconstructs.append(reconstruct)
+    return simis
 
+train_labels = ['beta-lactam', 'multidrug', 'bacitracin', 'MLS', 'aminoglycoside', 'polymyxin', 'tetracycline',    
+ 'fosfomycin', 'chloramphenicol', 'glycopeptide', 'quinolone', 'peptide','sulfonamide', 'trimethoprim', 'rifamycin',
+ 'qa_compound', 'aminocoumarin', 'kasugamycin', 'nitroimidazole', 'streptothricin', 'elfamycin', 'fusidic_acid',
+ 'mupirocin', 'tetracenomycin', 'pleuromutilin', 'bleomycin', 'triclosan', 'ethambutol', 'isoniazid', 'tunicamycin',
+ 'nitrofurantoin', 'puromycin', 'thiostrepton', 'pyrazinamide', 'oxazolidinone', 'fosmidomycin']
+prepare = sorted(train_labels)
+label_dic = {}
+for index, ele in enumerate(prepare):
+    label_dic[index] = ele
 
-cuts = [0.8275862068965517, 0.7, 0.6842105263157895]
+#cuts = [0.8275862068965517, 0.7, 0.6842105263157895]
+cut = 0.737265577737447
 
 def argnet_ssaa(input_file, outfile):
 
     with open(os.path.join(os.path.dirname(__file__), "../results/" + outfile) , 'a') as f:
             f.write('test_id' + '\t' + 'ARG_prediction' + '\t' + 'resistance_category' + '\t' + 'probability' + '\n')
-
+    print('reading in test file...')
     test = [i for i in sio.parse(input_file, 'fasta')]
-    test_ids = [ele.id for ele in test]
-    
-    train_data = [i for i in sio.parse(os.path.join(os.path.dirname(__file__), "../data/train.fasta"),'fasta')]
-    train_labels = [ele.id.split('|')[3].strip() for ele in train_data]
-    encodeder = LabelBinarizer()
-    encoded_train_labels = encodeder.fit_transform(train_labels)
-    prepare = sorted(list(set(train_labels)))
-    label_dic = {}
-    for index, ele in enumerate(prepare):
-        label_dic[index] = ele
-    
+
+    print('encoding test file...')
+    print('reconstruct, simi...')
     for idx, test_chunk in enumerate(list(chunks(test, 10000))):
         #print(str(idx) + 'th batch encoding...')
         testencode = testencode64(test_chunk)
         #print(str(idx) + 'th batch predict...')
         testencode_pre = filter_prediction_batch(testencode)
         #print(str(idx) + 'th reconstruct, simi...')
-        reconstructs, simis = reconstruction_simi(testencode_pre, test_chunk)
+        simis = reconstruction_simi(testencode_pre, test_chunk)
         passed_encode = [] ### notice list and np.array
         passed_idx = []
         notpass_idx = []
         for index, ele in enumerate(simis):
-            if ele >= cuts[0]:
+#            if ele >= cuts[0]:
+#                passed_encode.append(testencode[index])
+#                passed_idx.append(index)
+#            else:
+#                notpass_idx.append(index)
+#            if len(test[index]) in range(40, 50):
+#                if ele >= cuts[1]:
+#                    passed_encode.append(testencode[index])
+#                    passed_idx.append(index) 
+#                else:
+#                    notpass_idx.append(index)          
+#            if len(test[index]) == 50:
+#                if ele >= cuts[-1]:
+#                    passed_encode.append(testencode[index])
+#                    passed_idx.append(index)
+#                else:                
+#                    notpass_idx.append(index)
+            if ele >= cut:
                 passed_encode.append(testencode[index])
                 passed_idx.append(index)
             else:
                 notpass_idx.append(index)
-            if len(test[index]) in range(40, 50):
-                if ele >= cuts[1]:
-                    passed_encode.append(testencode[index])
-                    passed_idx.append(index) 
-                else:
-                    notpass_idx.append(index)          
-            if len(test[index]) == 50:
-                if ele >= cuts[-1]:
-                    passed_encode.append(testencode[index])
-                    passed_idx.append(index)
-                else:                
-                    notpass_idx.append(index)
     
     ###classification
+        print('classifying...')
         classifications = []
-        classifications = classifier.predict(np.stack(passed_encode, axis=0), batch_size = 3000) 
+        if len(passed_encode) > 0:
+            classifications = classifier.predict(np.stack(passed_encode, axis=0), batch_size = 3000)
+            out = {}
+            classification_argmax = np.argmax(classifications, axis=1)
+            classification_max = np.max(classifications, axis=1)
+
+        if len(passed_encode) == 0:
+            print('no seq passed!')
+            pass
 
         out = {}
         for i, ele in enumerate(passed_idx):
-            out[ele] = [np.max(classifications[i]), label_dic[np.argmax(classifications[i])]]
+            out[ele] = [classification_max[i], label_dic[classification_argmax[i]]]
 
         ### output
+        print('writing output...')
         with open(os.path.join(os.path.dirname(__file__), "../results/" + outfile) , 'a') as f:
             for idx, ele in enumerate(test_chunk):
                 if idx in passed_idx:
