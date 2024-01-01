9eb7359b5e0a520a30ee7021be8b478928694a89
scpeiyao <scpeiyao@gmail.com>
2024-01-01 12:14:41 +0800

    get_models bash

---
+++ model/get-models.sh
@@ -0,0 +1,4 @@
+wget https://github.com/patience111/ARGNet/releases/download/v1.0.1/AELS_tall.h5
+wget https://github.com/patience111/ARGNet/releases/download/v1.0.1/AESS_tall.h5
+wget https://github.com/patience111/ARGNet/releases/download/v1.0.1/classifier-ls_tall.h5
+wget https://github.com/patience111/ARGNet/releases/download/v1.0.1/classifier-ss_tall.h5
